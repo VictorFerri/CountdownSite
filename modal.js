d3795fd4c965406926edbc18b68424a3bc1a61fe
VictorFerri <victorferri_@hotmail.com>
2023-10-31 18:03:48 -0300

    Update the functions to can choose the date

--- modal.js
+++ modal.js
@@ -1,9 +1,19 @@
 const Modal = {
     open() {
         document.querySelector('.modalOverlay').classList.add('active')
+        console.log('get: ', localStorage.getItem('selected_date'));
+        document.getElementById('date').value = localStorage.getItem('selected_date') != null ? new Date(localStorage.getItem('selected_date')).toISOString().split('T')[0] :  new Date().toISOString().split('T')[0] ;
+        console.log(document.getElementById('date'));
+        document.getElementById('date').setAttribute('min', new Date().toISOString().split('T')[0])
     },
+
+    onChangeDate() {
+
+    },
+
     close() {
-        document
-            .querySelector('.modalOverlay').classList.remove('active')
+        localStorage.setItem('selected_date', document.getElementById('date').value);
+        document.querySelector('.modalOverlay').classList.remove('active')
+        window.location.reload();
     }
 }
--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 import Countdown from "./countdown.js";
 
-const tempoParaOWWB = new Countdown("1 January 3000 00:00:00 GMT-0300");
 const tempos = document.querySelectorAll(".data-time");
-console.log(tempoParaOWWB.total)
 
-function mostrarTempo() {
+const date = new Date(localStorage.getItem('selected_date'));
+
+const day = date.getDate();
+const month = date.toLocaleString('en-US', { month: 'long' });
+const year = date.getFullYear();
+
+const tempoParaOWWB = new Countdown(`${day} ${month} ${year} 00:00:00 GMT-0300`);
+
+function showTime() {
   tempos.forEach((tempo, index) => {
     tempo.innerHTML = tempoParaOWWB.total[index];
   });
 }
 
-mostrarTempo();
-setInterval(mostrarTempo, 1000);
+showTime();
+setInterval(showTime, 1000);
